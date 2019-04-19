99be515b4428e648c785eacca0d35341b48db469
XWHQSJ <xwhqsj@gmail.com>
2019-04-19 12:35:18 +0800

    add quadtree base implement

---
+++ cmake-build-debug/CMakeFiles/Quadtree.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/wanhuixu/Quadtree/src/bounds.c" "/home/wanhuixu/Quadtree/cmake-build-debug/CMakeFiles/Quadtree.dir/src/bounds.c.o"
+  "/home/wanhuixu/Quadtree/src/node.c" "/home/wanhuixu/Quadtree/cmake-build-debug/CMakeFiles/Quadtree.dir/src/node.c.o"
+  "/home/wanhuixu/Quadtree/src/point.c" "/home/wanhuixu/Quadtree/cmake-build-debug/CMakeFiles/Quadtree.dir/src/point.c.o"
+  "/home/wanhuixu/Quadtree/src/quadtree.c" "/home/wanhuixu/Quadtree/cmake-build-debug/CMakeFiles/Quadtree.dir/src/quadtree.c.o"
+  "/home/wanhuixu/Quadtree/test.c" "/home/wanhuixu/Quadtree/cmake-build-debug/CMakeFiles/Quadtree.dir/test.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../."
+  "../src"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/Quadtree.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Quadtree.dir/src/bounds.c.o"
+  "CMakeFiles/Quadtree.dir/src/node.c.o"
+  "CMakeFiles/Quadtree.dir/src/point.c.o"
+  "CMakeFiles/Quadtree.dir/src/quadtree.c.o"
+  "CMakeFiles/Quadtree.dir/test.c.o"
+  "Quadtree.pdb"
+  "Quadtree"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Quadtree.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
