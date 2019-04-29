9c724b9e7c6e9204636bde0cd0ef4e3c06dc6006
XWHQSJ <xwhqsj@gmail.com>
2019-04-29 20:48:32 +0800

    just add the main.c

--- main.c
+++ main.c
@@ -2,3 +2,26 @@
 // Created by Carillon on 2019/4/29.
 //
 
+#include "ore/ore.h"
+#include "ore/errors.h"
+#include "ore/flags.h"
+
+#include <stdio.h>
+#include <stdint.h>
+#include <time.h>
+
+static int _err;
+static double **pPointX;
+static double **pPointY;
+
+#define ERR_CHECK(x) if((_err = x) != ERROR_NONE) { return _err; }
+
+
+int main() {
+
+
+
+
+
+    return 0;
+}
